c2c14528b526a1f6f2254405d36097c6988a3f44
pavelsetsko <Pashech555@gmail.com>
2024-12-06 11:07:45 +0300

    added empty projeect

---
+++ src/test/java/com/example/security_jwt_example/SecurityJwtExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.security_jwt_example;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtExampleApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
